d78afe5064c65712965effffd9fcc803914929fa
Anna Pomichal <rucsna@gmail.com>
2024-07-18 09:25:31 +0200

    feat: add footer to Layout

--- CheckMateClient/src/Pages/Layout.jsx
+++ CheckMateClient/src/Pages/Layout.jsx
@@ -1,28 +1,25 @@
-import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Footer from "../Components/Footer";
 
-const Layout = ({children}) => {
-    const months = ['January', 'February', 'March', 'April'];
+const Layout = ({ children }) => {
+  const months = ['January', 'February', 'March', 'April'];
 
-    return (
-        <div>
-    <Navbar expand="lg" bg="info">
-      <Container fluid>
-        <Nav className="me-auto">
-            <Navbar.Brand href="#home">2024</Navbar.Brand>
-            <NavDropdown title='June' id="month-dropdown">
-            {months.map((month, index) => (
-              <NavDropdown.Item key={index}>{month}</NavDropdown.Item>
-            ))}
-            </NavDropdown> 
-        </Nav>
-      </Container>
-    </Navbar>
-    {children}
-    </div>
+  return (
+    <Container fluid>
+      <Nav className="bg-info flex-column">
+        <Nav.Link href="#home"><h1>2024</h1></Nav.Link>
+        <NavDropdown title='June' id="month-dropdown">
+          {months.map((month, index) => (
+            <NavDropdown.Item className="bg-info" key={index}>{month}</NavDropdown.Item>
+          ))}
+        </NavDropdown>
+      </Nav>
+      {children}
+      <Footer />
+    </Container>
   );
-}
+};
 
 export default Layout;
