3d806bc58985ba891add6d077574440b79f10062
Anna Pomichal <rucsna@gmail.com>
2024-07-18 23:38:09 +0200

    refactor: add formatting to component

--- CheckMateClient/src/Components/TaskList.jsx
+++ CheckMateClient/src/Components/TaskList.jsx
@@ -1,45 +1,46 @@
 import ListGroup from "react-bootstrap/ListGroup";
-import PropTypes from "prop-types";
 import Form from "react-bootstrap/Form";
+import { useContext } from "react";
+import { StateContext } from "../StateContext";
 
-const TaskList = ({tasks, setTasks}) => {
+const TaskList = () => {
+    const { tasks, setTasks } = useContext(StateContext);
+
     const handleTask = (id) => {
         console.log('id', id);
         const changedTasks = tasks.map(task => {
-            if(task.id === id){
+            if (task.id === id) {
                 console.log(task.id);
-                return {...task, completed: !task.completed};  
+                return { ...task, completed: !task.completed };
             };
-            return task;   
+            return task;
         });
-        
+
         setTasks(changedTasks);
     };
 
-    return(
+    return (
         <ListGroup>
             {
                 tasks.map((task) => (
                     <ListGroup.Item key={task.id}>
-                    <Form>
-                        <Form.Check 
-                            type="checkbox" 
-                            id={`task-${task.id}`} 
-                            label={task.name}
-                            checked={task.completed}
-                            disabled={task.completed}
-                            onChange={() => handleTask(task.id)}
+                        <Form className="d-flex align-items-center">
+                            <Form.Check
+                                type="checkbox"
+                                id={`task-${task.id}`}
+                                label={task.name}
+                                checked={task.completed}
+                                disabled={task.completed}
+                                onChange={() => handleTask(task.id)}
                             />
-                   </Form>
-                   </ListGroup.Item>
+                            <i className="bi bi-pencil-square ms-auto me-2"></i>
+                            <i className="bi bi-trash3-fill me-2"></i>
+                        </Form>
+                    </ListGroup.Item>
                 ))
             }
-      </ListGroup> 
+        </ListGroup>
     )
-};
-
-TaskList.propTypes = {
-    tasks: PropTypes.array.isRequired
 };
 
 export default TaskList;
