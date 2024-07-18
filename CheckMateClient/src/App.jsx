0a122a9419e69d5a045bc00f007b181f6473b2a8
Anna Pomichal <rucsna@gmail.com>
2024-07-18 09:20:10 +0200

    chore: add ContextProvider to App.jsx

--- CheckMateClient/src/App.jsx
+++ CheckMateClient/src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css'
-import MonthlyView from './Pages/MonthlyView'
+import './App.css';
+import MonthlyView from './Pages/MonthlyView';
+import { ContextProvider } from './StateContext';
 
 function App() {
 
   return (
+    <ContextProvider>
       <div>
         <MonthlyView />
       </div>
+    </ContextProvider>
   )
-}
+};
 
-export default App
+export default App;
