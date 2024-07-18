d37a69f63c847d7161ab2db67b3fd73f9ae1bcc5
Anna Pomichal <rucsna@gmail.com>
2024-07-18 09:26:54 +0200

    feat: add dynamicly changable week start option

--- CheckMateClient/src/Pages/MonthlyView.jsx
+++ CheckMateClient/src/Pages/MonthlyView.jsx
@@ -1,51 +1,65 @@
 import DayCard from "../Components/DayCard";
 import Layout from "./Layout";
 import Table from "react-bootstrap/Table";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import DailyView from "./DailyView";
+import AddTaskModal from "../Components/AddTaskModal";
+import { StateContext } from "../StateContext";
 
 const MonthlyView = () => {
-    const [daysInTheMonth, setDaysInTheMonth] = useState(30);
-    const [startDay, setStartDay] = useState(6);
-    const [weekNumber, setWeekNumber] = useState(26);
+    const { weekStart } = useContext(StateContext);
+
+    const [daysInTheMonth, setDaysInTheMonth] = useState(31);
+    const [startDay, setStartDay] = useState(0);
+    const [weekNumber, setWeekNumber] = useState(31);
+
+    const [dailyViewShow, setDailyViewShow] = useState(false);
+    const [addTaskShow, setAddTaskShow] = useState(false);
+
+    const days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thirsday", "Friday", "Saturday"];
+    const headers = weekStart === "M"
+        ? [...days.slice(1), days[0]]
+        : days;
 
     const totalCells = daysInTheMonth + startDay;
     const totalRows = Math.ceil(totalCells / 7);
     let current = weekNumber;
 
     useEffect(() => {
+        if(weekStart === "M"){
+            setStartDay(0);
+        } else {
+            setStartDay(0 + 1);
+        }
         // this useEffect will set the
         //      daysInTheMonth, startDay, weekNumber states upon first rendering
-    }, []);
+    }, [weekStart]);
     
 
     return (
-        <Layout>
+        <Layout >
             <Table striped bordered hover>
                 <thead>
                     <tr>
-                        <th>#</th>
-                        <th>Monday</th>
-                        <th>Tuesday</th>
-                        <th>Wednesday</th>
-                        <th>Thirsday</th>
-                        <th>Friday</th>
-                        <th>Saturday</th>
-                        <th>Sunday</th>
+                        <th><i className="bi bi-calendar-check"></i></th>
+                        {headers.map((day, index) => (
+                            <th key={index ++} className="text-info">{day}</th>
+                        ))}
                     </tr>
                 </thead>
                 <tbody>
                     {
                         Array.from({ length: totalRows }, (_, rowIndex) => (
                             <tr key={rowIndex}>
-                                <td>{current++}</td>
+                                <td className="text-info">{current++}</td>
                                 {
                                     Array.from({ length: 7 }, (_, colIndex) => {
                                         const dayIndex = rowIndex * 7 + colIndex;
                                         const dayNumber = dayIndex - startDay + 1;
                                         if (dayIndex < startDay || dayNumber > daysInTheMonth) return <td key={colIndex}></td>;
                                         return (
-                                            <td className="col-lg-1-7 col-md-4 col-sm-6 mb-4" key={colIndex}>
-                                                <DayCard title={dayNumber} />
+                                            <td className="col-lg-2 col-md-2 col-sm-2 mb-6" key={colIndex}>
+                                                <DayCard title={dayNumber} setModalShow={setDailyViewShow} />
                                             </td>
                                         );
                                     })
@@ -55,6 +69,8 @@ const MonthlyView = () => {
                     }
                 </tbody>
             </Table>
+            <DailyView show={dailyViewShow} onHide={() => setDailyViewShow(false)} daynumber={2} />
+            <AddTaskModal show={addTaskShow} onHide={() => setAddTaskShow(false)} daynumber={2} showdailyview={() => setDailyViewShow(true)} />
         </Layout>
     )
 }
