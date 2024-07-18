d0324ad0743e06a55fbdfc2bf4ade2a25b353e2d
Anna Pomichal <rucsna@gmail.com>
2024-07-18 09:28:48 +0200

    feat: add badges to show finished and unfinished tasks on DayCard

--- CheckMateClient/src/Components/DayCard.jsx
+++ CheckMateClient/src/Components/DayCard.jsx
@@ -1,19 +1,29 @@
 import Card from "react-bootstrap/Card";
+import Badge from "react-bootstrap/Badge";
+import PropTypes from "prop-types";
 
-const DayCard = ({title, info}) => {
-    return(
-        <Card>
-        <Card.Header>{title}</Card.Header>
-        <Card.Body>
-          <Card.Title>Statistics about daily tasks</Card.Title>
-          <Card.Text>
-            {info}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    )
+const DayCard = ({ title, setModalShow }) => {
+  return (
+    <Card className="bg-light">
+      <Card.Title onClick={() => setModalShow(true)}>
+        <h1>{title}</h1>
+      </Card.Title>
+
+      <Card.Body>
+        <Card.Text>
+          <Badge pill bg="success">3 <i className="bi bi-check2"></i></Badge>
+          <Badge pill bg="danger">2 <i className="bi bi-x-lg"></i></Badge>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  )
 
 
-}
+};
+
+DayCard.propTypes = {
+  title: PropTypes.number.isRequired,
+  setModalShow: PropTypes.func.isRequired,
+};
 
 export default DayCard;
