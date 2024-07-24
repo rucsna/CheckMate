d74eaaec4a3853277664ca87d4fe1f1d070661d5
Anna Pomichal <rucsna@gmail.com>
2024-07-24 13:49:15 +0200

    refactor: hoist variables and functions to global level

--- CheckMateClient/src/StateContext.jsx
+++ CheckMateClient/src/StateContext.jsx
@@ -3,9 +3,16 @@ import { createContext, useState } from "react";
 export const StateContext = createContext();
 
 export const ContextProvider = ({children}) => {
+    const getMonthName = (month) => new Date(1970, month, 1).toLocaleString('en-US', { month: 'long' });
+
+
+    const currentDate = new Date();
     const [weekStart, setWeekStart] = useState("M");
     const [mode, setMode] = useState();
-    const currentDate = new Date();
+    const [selectedYear, setSelectedYear] = useState(currentDate.getFullYear());
+    const [selectedMonth, setSelectedMonth] = useState(getMonthName(currentDate.getMonth()));
+    const [selectedDay, setSelectedDay] = useState(currentDate.getDate());
+    const [isToday, setIsToday] = useState(false);    
 
     const [tasks, setTasks] = useState([
         { id: 0, name: "feed the cat", completed: false, date: "2024.07.03" },
@@ -14,7 +21,24 @@ export const ContextProvider = ({children}) => {
         { id: 3, name: "call the plumber", completed: false, date: "2024.07.25" }]);
 
     return(
-        <StateContext.Provider value={{weekStart, setWeekStart, mode, setMode, tasks, setTasks, currentDate}}>
+        <StateContext.Provider value={{
+            weekStart, 
+            setWeekStart, 
+            mode, 
+            setMode, 
+            tasks, 
+            setTasks, 
+            currentDate,
+            selectedYear, 
+            setSelectedYear,
+            selectedMonth,
+            setSelectedMonth,
+            selectedDay,
+            setSelectedDay,
+            getMonthName,
+            isToday,
+            setIsToday
+            }}>
             {children}
         </StateContext.Provider>
     )
