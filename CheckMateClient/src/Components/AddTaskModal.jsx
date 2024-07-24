f5c1d732091270a80f9a5f1e54dee87e9d035aa4
Anna Pomichal <rucsna@gmail.com>
2024-07-24 13:50:33 +0200

    refactor: omit unused parts of the modal

--- CheckMateClient/src/Components/AddTaskModal.jsx
+++ CheckMateClient/src/Components/AddTaskModal.jsx
@@ -1,38 +1,22 @@
 import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/esm/Button";
-import Row from "react-bootstrap/esm/Row";
-import Col from "react-bootstrap/esm/Col";
 import TaskForm from "./TaskForm";
 
 const AddTaskModal = (props) => {
-
-    const handleDailyViewModal = () => {
-        props.showdailyview();
-        props.onHide();
-    };
-
     return (
         <Modal
             {...props}
-            size="lg"
+            size="md"
             aria-labelledby="contained-modal-title-vcenter"
             centered
         >
-            <Modal.Header>
+            <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
                     <h1 className="text-info">What should I do today?</h1>
                 </Modal.Title>
             </Modal.Header>
-            <Modal.Body>
+            <Modal.Body className="bg-info">
                 <TaskForm />
             </Modal.Body>
-            <Modal.Footer>
-                <Row className="w-100">
-                    <Col className="text-end">
-                        <Button onClick={handleDailyViewModal}>Back to day {props.daynumber}</Button>
-                    </Col>
-                </Row>
-            </Modal.Footer>
         </Modal>
     )
 }
