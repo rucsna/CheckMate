9184b9dfbd441c1b41036e97abf70bbdee7625cc
Anna Pomichal <rucsna@gmail.com>
2024-07-24 13:56:05 +0200

    refactor: add design changes

--- CheckMateClient/src/Pages/Layout.jsx
+++ CheckMateClient/src/Pages/Layout.jsx
@@ -7,17 +7,27 @@ import { useContext } from "react";
 import { StateContext } from "../StateContext";
 import Button from "react-bootstrap/esm/Button";
 
-const Layout = ({ children, handleMonthChange, selectedMonth }) => {
-  const { currentDate } = useContext(StateContext);
 
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+
+const Layout = ({ children, handleMonthChange }) => {
+  const { currentDate, selectedMonth, setIsToday } = useContext(StateContext);
+
+
+  const handleClick = () => {
+    setIsToday(true);
+    children.setDailyViewShow(true);
+  };
+
 
   return (
     <Container fluid className="d-flex flex-column" style={{ height: "100vh" }}>
       <Nav className="bg-info flex-column">
         <div className="d-flex align-items-center">
-          <NavItem className="ms-5 mt-4"><h1>2024</h1></NavItem>
-          <Button className="ms-auto me-5 mt-4" onClick={() => children.setDailyViewShow(true)}>Today</Button>
+          <NavItem className="ms-5 mt-2"><h1>{currentDate.getFullYear()}</h1></NavItem>
+          <Button variant="warning" className="ms-auto me-5 mt-4 text-primary shadow" onClick={() => children.setAddTaskShow(true)}>New todo</Button>
+          <Button className="me-5 mt-4 shadow" onClick={handleClick}>Today</Button>
         </div>
         <NavDropdown className="ms-5" title={selectedMonth} id="month-dropdown" onSelect={handleMonthChange}>
           {months.map((month, index) => (
@@ -26,7 +36,7 @@ const Layout = ({ children, handleMonthChange, selectedMonth }) => {
         </NavDropdown>
       </Nav>
       {children.component}
-      <Footer />
+      <Footer setStartDay={children.setStartDay}/>
     </Container>
   );
 };
