a9e0fcff957e89ad0ab1aaa7fcf3976cc09c98a0
Anna Pomichal <rucsna@gmail.com>
2024-08-02 08:54:52 +0200

    chore: add CORS policy to Program.cs

--- CheckMateBackend/CheckMateBackend/Program.cs
+++ CheckMateBackend/CheckMateBackend/Program.cs
@@ -4,7 +4,15 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("Todos") ?? "Data Source=Todo.db";
+var connectionString = builder.Configuration.GetConnectionString("Todos") ?? "Data Source=todo.db";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
 
 builder.Services.AddSqlite<TodoDb>(connectionString);
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -20,16 +28,30 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        DatabaseSeeder.Initialize(services);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"An error occured seeding the database {e.Message}");
+    }
+}
+
+app.UseCors("AllowSpecificOrigin");
 
 var todoItems = app.MapGroup("/api/todos");
 
 todoItems.MapGet("/", GetAllTodos);
+todoItems.MapGet("/{year:int}/{month:int}", GetTodosByMonth);
 todoItems.MapGet("/{date}", GetTodosByDate);
 todoItems.MapGet("/{id:int}", GetTodo);
 todoItems.MapPost("/", CreateTodo);
 todoItems.MapPut("/{id:int}", UpdateTodo);
 todoItems.MapDelete("/{id:int}", DeleteTodo);
-
 
 app.UseHttpsRedirection();
 
@@ -48,6 +70,13 @@ static async Task<IResult> GetTodosByDate(TodoDb db, string date)
         return TypedResults.Ok(todos);
     }
     return TypedResults.BadRequest("Invalid date format");
+}
+
+static async Task<IResult> GetTodosByMonth(TodoDb db, int year, int month)
+{
+    var todos = await db.TodoItems.Where(todo => todo.Date.Year == year && todo.Date.Month == month)
+        .ToListAsync();
+    return TypedResults.Ok(todos);
 }
 
 static async Task<IResult> GetTodo(TodoDb db, int id)
