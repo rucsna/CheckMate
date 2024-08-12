08cb764a46976f53531c4986b62ed56c2b117f98
Anna Pomichal <rucsna@gmail.com>
2024-08-12 09:27:32 +0200

    feat: create database with initial data

--- CheckMateBackend/CheckMateBackend/DatabaseSeeder.cs
+++ CheckMateBackend/CheckMateBackend/DatabaseSeeder.cs
@@ -13,12 +13,12 @@ public static class DatabaseSeeder
         context.TodoItems.AddRange(
             new Todo { Name = "feed the cat", IsCompleted = false, Date = DateTime.Now.AddDays(-10) },
             new Todo { Name = "create backend", IsCompleted = true, Date = DateTime.Now.AddDays(-8) },
-            new Todo { Name = "add database", IsCompleted = true, Date = DateTime.Now.AddDays(-7) },
-            new Todo { Name = "buy train tickets", Date = DateTime.Now.AddDays(-7) },
-            new Todo { Name = "feed the dog, too", IsCompleted = true, Date = DateTime.Now.AddDays(-5) },
-            new Todo { Name = "call the plumber", Date = DateTime.Now.AddDays(-5) },
+            new Todo { Name = "add database", IsCompleted = true, Date = DateTime.Now.AddDays(7) },
+            new Todo { Name = "buy train tickets", Date = DateTime.Now.AddDays(7) },
+            new Todo { Name = "feed the dog, too", IsCompleted = true, Date = DateTime.Now.AddDays(6) },
+            new Todo { Name = "call the plumber", Date = DateTime.Now.AddDays(5) },
             new Todo { Name = "do presentation", Date = DateTime.Now.AddDays(-2) },
-            new Todo { Name = "call Granny", Date = DateTime.Now.AddDays(-1) },
+            new Todo { Name = "call Granny", Date = DateTime.Now },
             new Todo { Name = "drink more water", Date = DateTime.Now },
             new Todo { Name = "write tests", Date = DateTime.Now.AddDays(1) },
             new Todo { Name = "frontend tests, too!!", Date = DateTime.Now.AddDays(3) }
