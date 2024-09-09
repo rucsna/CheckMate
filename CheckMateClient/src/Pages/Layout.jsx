3cd18aa5a951ad3f2604d934025a28dd751b8566
Anna Kemény <rucsna@gmail.com>
2024-09-09 10:36:34 +0200

    refactor: adjust Layout and MonthlyView to the changes in App.jsx

--- CheckMateClient/src/Pages/Layout.jsx
+++ CheckMateClient/src/Pages/Layout.jsx
@@ -1,109 +1,39 @@
 import { useContext, useState } from "react";
-import { DateContext } from "../Contexts/DateContext";
-import { SettingsContext } from "../Contexts/SettingsContext";
-import { Container, Nav, NavDropdown, Card, Button } from "react-bootstrap";
-import Footer from "../Components/Footer";
+import { TaskContext } from "../Contexts/TaskContext";
+import DailyView from "./DailyView";
+import AddTaskModal from "../Components/AddTaskModal";
+import Footer from "../Components/Layout/Footer";
 import CurrentTaskDisplayer from "../Components/CurrentTaskDisplayer";
 import PropTypes from "prop-types";
+import NavBar from "../Components/Layout/NavBar";
 
 
-const Layout = ({ children, handleMonthChange }) => {
-  const { currentDate, selectedMonth, setIsToday, setSelectedMonth, setSelectedYear, setSelectedDay, selectedYear } = useContext(DateContext);
-  const { monthNames, labels } = useContext(SettingsContext);
+const Layout = ({ children }) => {
+  const { dailyViewShow, setDailyViewShow } = useContext(TaskContext);
 
-  const [dropdownSelectedMonth, setDropdownSelectedMonth] = useState(currentDate.getMonth());
-  const [isLeftDisabled, setIsLeftDisabled] = useState(false);
-  const [isRightDisabled, setIsRightDisabled] = useState(false);
-
-
-  const handleTodaysTaskClick = () => {
-    setIsToday(true);
-    children.setDailyViewShow(true);
-  };
-
-  const handleNewTodoClick = () => {
-    setIsToday(true);
-    children.setAddTaskShow(true);
-  }
-
-  const handleYearChange = (step) => {
-    const newYear = Number(selectedYear) + step;
-    const minYear = currentDate.getFullYear() - 100;
-    const maxYear = currentDate.getFullYear() + 100;
-
-    setIsLeftDisabled(newYear <= minYear);
-    setIsRightDisabled(newYear >= maxYear);
-
-    selectedYear(newYear);
-    handleMonthChange(selectedMonth, newYear);
-  }
-
-  const handleYearLeftClick = () => {
-    handleYearChange(-1);
-  };
-
-  const handleYearRightClick = () => {
-    handleYearChange(1);
-  };
-
-  const setBackToday = () => {
-    setSelectedYear(currentDate.getFullYear());
-    handleMonthChange(currentDate.getMonth());
-    setSelectedMonth(currentDate.getMonth());
-    setSelectedDay(currentDate.getDate());
-  };
-
-  const handleDropdown = (eventKey) => {
-    setDropdownSelectedMonth(Number(eventKey));
-    handleMonthChange(Number(eventKey), selectedYear);
-  };
+  const [addTaskShow, setAddTaskShow] = useState(false);
 
 
   return (
-    <Container fluid className="d-flex flex-column" style={{ height: "100vh" }}>
-      <Nav className="bg-success bg-opacity-75 d-flex justify-content-between align-items-center">
+    <div className="layout-container">
+      <header>
+        <NavBar setAddTaskShow={setAddTaskShow}/>
+      </header>
 
-        <Card className="ms-5 mt-2 mb-2 bg-secondary bg-opacity-50 text-warning d-flex align-items-center shadow-lg">
-          <Card.Text className="mt-2" as="h1">
-            <span>
-              <Button variant="link" onClick={handleYearLeftClick} disabled={isLeftDisabled}><i className="bi bi-caret-left-fill"></i></Button>
-              {selectedYear}
-              <Button variant="link" onClick={handleYearRightClick} disabled={isRightDisabled}><i className="bi bi-caret-right-fill"></i></Button>
-            </span>
-          </Card.Text>
+      <main>
+        <CurrentTaskDisplayer />
+        {children}
+        <DailyView show={dailyViewShow} onHide={() => setDailyViewShow(false)} />
+        <AddTaskModal show={addTaskShow} onHide={() => setAddTaskShow(false)} />
+      </main>
 
-          <NavDropdown
-            title={<span className="text-warning">{monthNames[dropdownSelectedMonth]}</span>}
-            id="month-dropdown"
-            onSelect={(eventKey) => handleDropdown(eventKey)}
-            className="month-dropdown"
-          >
-            {monthNames.map((month, index) => (
-              <NavDropdown.Item className="bg-success bg-opacity-50" key={index} eventKey={index}>{month}</NavDropdown.Item>
-            ))}
-          </NavDropdown>
-        </Card>
-
-        <div className="d-flex align-items-center justify-content-center">
-          <Button variant="outline-dark" className="ms-auto me-4 bg-warning text-primary shadow-lg" onClick={handleNewTodoClick}>{labels.newTodoButton}</Button>
-          <Button variant="outline-dark" className="me-5 bg-secondary bg-opacity-75 text-warning shadow-lg" onClick={handleTodaysTaskClick}>{labels.todaysTasks}</Button>
-        </div>
-
-        {/* <Card className="me-5 bg-warning text-secondary d-flex align-items-center shadow-lg">
-          <Card.Body>TODAY</Card.Body>
-        </Card> */}
-        
-      </Nav>
-      <CurrentTaskDisplayer />
-      {children.component}
-      <Footer setStartDay={children.setStartDay} />
-    </Container >
+      <Footer />
+    </div >
   );
 };
 
 Layout.propTypes = {
   children: PropTypes.any,
-  handleMonthChange: PropTypes.func.isRequired
 };
 
 export default Layout;
--- CheckMateClient/src/Pages/MonthlyView.jsx
+++ CheckMateClient/src/Pages/MonthlyView.jsx
@@ -1,37 +1,12 @@
-import { useState, useEffect, useContext } from "react";
+import { useEffect, useContext } from "react";
 import { DateContext } from "../Contexts/DateContext";
 import { SettingsContext } from "../Contexts/SettingsContext";
-import Layout from "./Layout";
-import DailyView from "./DailyView";
-import AddTaskModal from "../Components/AddTaskModal";
 import MonthTable from "../Components/MonthlyView/MonthTable";
 
 
-// Initializing functions for setting the selected month data
-const monthLengthCounter = (month, year) => new Date(year, month, 0).getDate();
-
-const getStartDay = (month, year, day) => new Date(year, month, day).getDay();
-
-const getWeekNumber = (month, year) => {
-    var currDate = new Date(year, month, 1);
-    var oneJan = new Date(year, 0, 1);
-    var numberOfDays = Math.floor((currDate - oneJan) / (24 * 60 * 60 * 1000));
-    return Math.ceil((currDate.getDay() + 1 + numberOfDays) / 7);
-}
-
-
 const MonthlyView = () => {
-    const { currentDate, setSelectedMonth, selectedMonth, selectedYear } = useContext(DateContext);
+    const { setStartDay, startDayIfMonday, startDayIfSunday} = useContext(DateContext);
     const { weekStart } = useContext(SettingsContext);
-
-    const [daysInTheMonth, setDaysInTheMonth] = useState(monthLengthCounter(currentDate.getMonth() + 1, currentDate.getFullYear()));
-    const [startDay, setStartDay] = useState(0);
-    const [startDayIfMonday, setStartDayIfMonday] = useState(getStartDay(currentDate.getMonth(), currentDate.getFullYear(), 1) - 1);
-    const [startDayIfSunday, setStartDayIfSunday] = useState(getStartDay(currentDate.getMonth(), currentDate.getFullYear(), 1));
-    const [weekNumber, setWeekNumber] = useState(getWeekNumber(currentDate.getMonth(), currentDate.getFullYear()));
-
-    const [dailyViewShow, setDailyViewShow] = useState(false);
-    const [addTaskShow, setAddTaskShow] = useState(false);
 
 
     useEffect(() => {
@@ -40,48 +15,12 @@ const MonthlyView = () => {
         } else {
             setStartDay(startDayIfSunday);
         }
-    }, [weekStart, startDay, startDayIfMonday, startDayIfSunday]);
-
-
-    const handleMonthChange = (monthIndex, year = selectedYear) => {
-        const lengthOfMonth = monthLengthCounter(Number(monthIndex) + 1, year);
-        const day1 = getStartDay(Number(monthIndex), year, 1) - 1;
-        const day2 = getStartDay(Number(monthIndex), year, 1);
-
-        day1 < 0 ? setStartDayIfMonday(6) : setStartDayIfMonday(day1);
-        setStartDayIfSunday(day2);
-        weekStart === "M" ? setStartDay(startDayIfMonday) : setStartDay(startDayIfSunday);
-       
-        setSelectedMonth(monthIndex);
-        setWeekNumber(getWeekNumber(monthIndex, year));
-        setDaysInTheMonth(lengthOfMonth);
-    };
+    }, [weekStart, startDayIfMonday, startDayIfSunday, setStartDay]);
 
 
     return (
         <div className="table-container">
-            <Layout handleMonthChange={handleMonthChange} selectedMonth={selectedMonth}>
-                {{
-                    component: (
-                        <div className="table-wrapper">
-                            <MonthTable
-                                daysInTheMonth={daysInTheMonth}
-                                startDay={startDay}
-                                weekNumber={weekNumber}
-                                setDailyViewShow={setDailyViewShow}
-                            />
-
-                            <DailyView show={dailyViewShow} onHide={() => setDailyViewShow(false)} />
-                            <AddTaskModal show={addTaskShow} onHide={() => setAddTaskShow(false)} />
-
-                        </div>
-                    ),
-
-                    setDailyViewShow: setDailyViewShow,
-                    setAddTaskShow: setAddTaskShow,
-                    setStartDay: setStartDay,
-                }}
-            </Layout>
+            <MonthTable/>
         </div>
     )
 };
